db3887f7642c13141312727a02ae8bb545463a3b
VU QUANG HUY <huyvq@bap.jp>
2017-10-05 16:14:34 +0700

    Update func

--- functions.php
+++ functions.php
@@ -1,12 +1,16 @@
 <?php 
 require get_template_directory() . '/resize.php';
 function hk_get_thumb($id, $w, $h){
-	$url = wp_get_attachment_url( get_post_thumbnail_id($id));
+	if(get_post_thumbnail_id($id)){
+		$url = wp_get_attachment_url( get_post_thumbnail_id($id));
+	} else {
+		$url = get_bloginfo('template_directory').'/no-thumb.jpg';
+	}                                                        
 	$image = huykira_image_resize($url, $w, $h, true, false);
 	return $image['url'];	
 }
 
-function hk_get_image($url, $w, $h){
+function hk_get_image($url, $w, $h){                                                        
 	$image = huykira_image_resize($url, $w, $h, true, false);
 	return $image['url'];	
 }
